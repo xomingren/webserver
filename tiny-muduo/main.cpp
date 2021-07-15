7b6dc5fa286303129b1d27128176e90b5a134020
AkiyamaMio <AkiyamaMio@kon.com>
2021-07-14 22:10:27 -0700

    echo server with epoll in main function

--- tiny-muduo/main.cpp
+++ tiny-muduo/main.cpp
@@ -1,7 +1,122 @@
-﻿#include <cstdio>
+﻿#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h> //for bzero
+#include <iostream>
+#include <unistd.h>
+using namespace std;
 
-int main()
+const uint8_t MAX_LINE = 100;
+const uint16_t MAX_EVENTS = 500;
+const uint8_t MAX_LISTENFD = 5;
+
+int createAndListen()
 {
-    printf("%s 向你问好!\n", "tiny_muduo");
+    int on = 1;
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in servaddr;
+    fcntl(listenfd, F_SETFL, O_NONBLOCK); //no-block io
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(11111);
+
+    if (-1 == bind(listenfd, (sockaddr*)&servaddr, sizeof(servaddr)))
+    {
+        cout << "bind error, errno:" << errno << endl;
+    }
+
+    if (-1 == listen(listenfd, MAX_LISTENFD))
+    {
+        cout << "listen error, errno:" << errno << endl;
+    }
+    return listenfd;
+}
+
+int main(int args, char** argv)
+{
+    epoll_event event, events[MAX_EVENTS];
+    int listenfd, connfd, sockfd;
+    ssize_t readlength;
+    char line[MAX_LINE];
+    sockaddr_in cliaddr;
+    socklen_t clilen = sizeof(sockaddr_in);
+    int epollfd = epoll_create(1);
+    if (epollfd < 0)
+        cout << "epoll_create error, error:" << epollfd << endl;
+    listenfd = createAndListen();
+    event.data.fd = listenfd;
+    event.events = EPOLLIN;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, listenfd, &event);
+
+    for (;;)
+    {
+        cout << "epoll waiting..." << endl;
+        int fds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+        if (fds == -1)
+        {
+            cout << "epoll_wait error, errno:" << errno << endl;
+            break;
+        }
+        for (int i = 0; i < fds; i++)
+        {
+            if (events[i].data.fd == listenfd)
+            {
+                connfd = accept(listenfd, (sockaddr*)&cliaddr, (socklen_t*)&clilen);
+                if (connfd > 0)
+                {
+                    cout << "new connection from "
+                        << "[" << inet_ntoa(cliaddr.sin_addr)
+                        << ":" << ntohs(cliaddr.sin_port) << "]"
+                        << " accept socket fd:" << connfd
+                        << endl;
+                }
+                else
+                {
+                    cout << "accept error, connfd:" << connfd
+                        << " errno:" << errno << endl;
+                }
+                fcntl(connfd, F_SETFL, O_NONBLOCK); //no-block io
+                epoll_event ev;
+                ev.data.fd = connfd;
+                ev.events = EPOLLIN;
+                ev.events |= EPOLLET;//egde 
+                if (-1 == epoll_ctl(epollfd, EPOLL_CTL_ADD, connfd, &ev))
+                    cout << "epoll_ctrl error, errno:" << errno << endl;
+            }
+            else if (events[i].events & EPOLLIN)
+            {
+                if ((sockfd = events[i].data.fd) < 0)
+                {
+                    cout << "EPOLLIN sockfd < 0 error " << endl;
+                    continue;
+                }
+                bzero(line, MAX_LINE);
+                readlength = read(sockfd, line, MAX_LINE);
+                if (readlength < 0)
+                {
+                    if (errno == ECONNRESET)
+                    {
+                        cout << "ECONNREST closed socket fd:" << events[i].data.fd << endl;
+                        close(sockfd);
+                    }
+                }
+                else if (readlength == 0)
+                {
+                    cout << "read 0 closed socket fd:" << events[i].data.fd << endl;
+                    close(sockfd);
+                }
+                else
+                {
+                    if (write(sockfd, line, readlength) != readlength)
+                        cout << "error: not finished one time" << endl;
+                    string s = line;
+                    cout << s << endl;
+                }
+            }
+        }
+    }
     return 0;
 }
